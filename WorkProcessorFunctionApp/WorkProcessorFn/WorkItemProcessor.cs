193c6eba573b311a987a042751b61c3021de07f6
Brent Stineman <brent@stinemans.org>
2018-08-22 16:33:54 -0700

    add batch input/output to event hub binding

--- WorkProcessorFunctionApp/WorkProcessorFn/WorkItemProcessor.cs
+++ WorkProcessorFunctionApp/WorkProcessorFn/WorkItemProcessor.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.ServiceBus;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
 using System.Text;
 
 namespace WorkProcessorFn
@@ -11,8 +12,9 @@ namespace WorkProcessorFn
     public static class WorkItemProcessor
     {
         [FunctionName("WorkItemProcessor")]
-        [return: EventHub("outputEventHubMessage", Connection = "EventHubConnectionAppSetting")]
-        public static void Run([EventHubTrigger("samples-workitems", Connection = "")] EventData[] myEventHubMessages, ILogger log)
+        public static async void Run([EventHubTrigger("samples-workitems", Connection = "")] EventData[] myEventHubMessages,
+            [EventHub("%EventHub%", Connection = "connectionEventHub")] IAsyncCollector<EventData> outputEventHubMessages,
+            ILogger log)
         {
             // process messages
             foreach (EventData message in myEventHubMessages)
@@ -23,14 +25,34 @@ namespace WorkProcessorFn
                     // process each message
                     var myEvent = JsonConvert.DeserializeObject(messagePaylog);
 
-                    // do something with it
+                    try
+                    {
+                        // modify message here
+                        await outputEventHubMessages.AddAsync(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        log.LogError(ex.Message);
+                        // do something else with it so its not lost
+                    }
+
+                }
+
+                // flush the event hub output, we're doing this and not letting the system so we can do error handling
+                // note that output batch must not exceed maximum Event Hub batch size
+                try
+                {
+                    await outputEventHubMessages.FlushAsync();
+                }
+                catch (Exception ex)
+                {
+                    log.LogError(ex.Message);
+                    //await Task.Delay(5000);
+                    // do something else with it so its not lost
                 }
 
             }
 
-            // gather all results
-
-            // write to output binding
         }
     }
 }
