f94ae74e765ab0cec5143dc09943aa1e640c524a
Brent Stineman <brent@stinemans.org>
2018-08-23 14:44:46 -0700

    adding simulated calc to workitemprocessor

--- WorkProcessorFunctionApp/WorkProcessorFn/WorkItemProcessor.cs
+++ WorkProcessorFunctionApp/WorkProcessorFn/WorkItemProcessor.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace WorkProcessorFn
@@ -17,6 +18,8 @@ namespace WorkProcessorFn
             [EventHub("%ResultsEH-Name%", Connection = "ResultsEH-ConnectionString")] IAsyncCollector<EventData> outputEventHubMessages,
             ILogger log)
         {
+            MD5 md5 = MD5.Create();
+
             // process messages
             foreach (EventData message in myEventHubMessages)
             {
@@ -35,7 +38,16 @@ namespace WorkProcessorFn
                         ResultList = new List<OffenderResult>()
                     };
 
+                    // simulated caluclation on payload
+                    byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(messagePayload));
+                    string hashStr = System.Text.Encoding.Default.GetString(hash);
+                    // craft outbound event
+                    OffenderResult newResult = new OffenderResult();
+                    newResult.Forensic_ID = hashStr;
+                    result.ResultList.Add(newResult);
+
                     EventData outputEvent = new EventData(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(result)));
+
                     outputEventHubMessages.AddAsync(outputEvent).Wait();
                 }
                 catch (Exception ex)
